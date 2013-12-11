7e91c465e0b347b1247ed57e90c8695b83a4a975
Frank Cangialosi <fcangialosi94@gmail.com>
2013-12-11 02:24:54 -0500

    why isnt this working :(

--- server.py
+++ server.py
@@ -4,12 +4,13 @@ import socket
 import os
 import sys
 import subprocess
+import re
 
 TCP_IP = '128.8.126.92'
 TCP_PORT = 8080
 BUFFER_SIZE = 64
 ping_count = 0
-NUM_PINGS = 30
+NUM_PINGS = 20
 
 def ping(ip):
     regex = re.compile("(\d+.\d+)")
@@ -24,21 +25,30 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
+global ip_of_x
+ip_of_x = ''
+
 conn, addr = s.accept()
 print 'Connection address:', addr
 for i in range(NUM_PINGS):
   while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data : break
-    print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)
+    print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)    
+    if i == 1:
+        ip_of_x = data
+        print ip_of_x
     conn.send(data)
     ping_count += 1
 conn.close()
 
 print "===="
-print "======== Pinging", data, "========"
+print "======== Pinging", ip_of_x, "========"
 print "===="
-r_xd = str(ping(data))
+r_xd = str(ping(ip_of_x))
+
+global ip_of_y
+ip_of_y = ''
 
 conn, addr = s.accept()
 print 'Connection address:', addr
@@ -47,6 +57,9 @@ for i in range(NUM_PINGS):
     data = conn.recv(BUFFER_SIZE)
     if not data : break
     print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)
+    if i == 1:
+        ip_of_y = data
+        print ip_of_y
     if i == (NUM_PINGS-1):
         conn.send(r_xd)
     else:
@@ -55,9 +68,9 @@ for i in range(NUM_PINGS):
 conn.close()
 
 print "===="
-print "======== Pinging", data, "========"
+print "======== Pinging", ip_of_y, "========"
 print "===="
-r_zd = str(ping(data))
+r_zd = str(ping(ip_of_y))
 
 conn, addr = s.accept()
 print 'Connection address:', addr
