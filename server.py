6cfcb243eb04c4a7275eafe5a69e2807646e788d
Frank Cangialosi <fcangialosi@studyblue.com>
2013-12-13 23:27:31 -0500

    updated server code

--- server.py
+++ server.py
@@ -1,88 +1,76 @@
 #!/usr/bin/evn python
 
 import socket
-import os
-import sys
 import subprocess
 import re
 
 TCP_IP = '128.8.126.92'
 TCP_PORT = 8080
-BUFFER_SIZE = 64
+BUFFER_SIZE = 128
 ping_count = 0
 NUM_PINGS = 20
 
 def ping(ip):
-    regex = re.compile("(\d+.\d+)")
+    pings = []
+    regex = re.compile("(\d+.\d+) ms")
     cmd = ['ping', '-c',str(NUM_PINGS),ip]
     p = subprocess.Popen(cmd,stdout=subprocess.PIPE)
-    line = p.stdout.readlines()[-1]
-    stats = regex.findall(line)
+    for line in p.stdout.readlines():
+        ping = regex.findall(line)
+        if ping != []:
+            pings.append(int(float(ping[0])))
     p.wait()
-    return stats
+    pings = pings[:-1]
+    return pings
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-global ip_of_x
-ip_of_x = ''
+while 1:
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        print data
+        if not data : break
+        print "=========================="
+        print "==== Pinging X from D ===="
+        print "=========================="
+        results = ping(data)
+        print results
+        conn.send(str(results))
+    conn.close()
 
-conn, addr = s.accept()
-print 'Connection address:', addr
-for i in range(NUM_PINGS):
-  while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data : break
-    print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)    
-    if i == 1:
-        ip_of_x = data
-        print ip_of_x
-    conn.send(data)
-    ping_count += 1
-conn.close()
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    for i in range(NUM_PINGS):
+        while 1:
+            data = conn.recv(BUFFER_SIZE)
+            if not data : break
+            print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)
+            conn.send(data)
+            ping_count += 1
+    conn.close()
 
-print "===="
-print "======== Pinging", ip_of_x, "========"
-print "===="
-r_xd = str(ping(ip_of_x))
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    for i in range(NUM_PINGS):
+        while 1:
+            data = conn.recv(BUFFER_SIZE)
+            if not data : break
+            print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)
+            conn.send(data)
+            ping_count += 1
+    conn.close()
 
-global ip_of_y
-ip_of_y = ''
-
-conn, addr = s.accept()
-print 'Connection address:', addr
-for i in range(NUM_PINGS):
-  while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data : break
-    print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)
-    if i == 1:
-        ip_of_y = data
-        print ip_of_y
-    if i == (NUM_PINGS-1):
-        conn.send(r_xd)
-    else:
-        conn.send(data)
-    ping_count += 1
-conn.close()
-
-print "===="
-print "======== Pinging", ip_of_y, "========"
-print "===="
-r_zd = str(ping(ip_of_y))
-
-conn, addr = s.accept()
-print 'Connection address:', addr
-for i in range(NUM_PINGS):
-  while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data : break
-    print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)
-    if i == (NUM_PINGS-1):
-        conn.send(r_zd)
-    else:
-        conn.send(data)
-    ping_count += 1
-conn.close()
-
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    for i in range(NUM_PINGS):    
+        while 1:
+            data = conn.recv(BUFFER_SIZE)
+            if not data : break
+            print '{0} bytes from {1}: ping_num={2}'.format(BUFFER_SIZE,addr,ping_count)
+            conn.send(data)
+            ping_count += 1
+    conn.close()
